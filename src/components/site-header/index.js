a91e96e311839b3327f18b0227545ac1bdeef1e2
elysia <elysia.hwang@gmail.com>
2020-12-30 23:45:36 -0500

    Add initial navigation.

--- src/components/site-header/index.js
+++ src/components/site-header/index.js
@@ -3,6 +3,12 @@ import React from "react";
 
 import "./style.scss";
 
+const NavLink = props => (
+  <li className="nav__item">
+    <Link to={props.to} className="nav__link">{props.children}</Link>
+  </li>
+)
+
 const Header = () => {
   return (
   <header>
@@ -12,6 +18,11 @@ const Header = () => {
         className="logomark">
           eh.
         </Link>
+
+        <ul className="nav__menu">
+          <NavLink to="/work">Work</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </ul>
     </nav>
   </header>
   )
