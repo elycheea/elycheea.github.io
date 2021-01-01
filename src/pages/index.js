5f6ee9a0f9f8458e6b8d9eb73d968e31d569d2a7
elysia <elysia.hwang@gmail.com>
2021-01-01 13:40:03 -0500

    Update intro content.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,19 @@ import * as React from "react"
 import ContentColumn from "../components/content-column";
 import Page from "../templates/page";
 
+import "./index.scss";
+
 const Home = () => {
   return (
     <Page>
       <ContentColumn>
-        <h1>Elysia Hwang</h1>
+        <h2 className="tagline">
+          <span className="rwd-line">Designer who loves code // </span>
+          <span className="rwd-line">or developer who loves design</span>
+        </h2>
+        <p className="intro">
+          I’m Elysia Hwang, a creative technologist with a growing interest in design and frontend&nbsp;ops. I’m based in Atlanta, GA.
+        </p>
       </ContentColumn>
     </Page>
   )
