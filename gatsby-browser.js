37bdc7192b5c165b094bd89170e09b68e33360bb
elysia <elysia.hwang@gmail.com>
2021-01-01 11:55:25 -0500

    Add Roboto Mono in 300.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,5 @@ import 'cssremedy/css/remedy.css';
 import 'cssremedy/css/reminders.css';
 import "fontsource-inter";
 import "fontsource-roboto-mono";
+import "fontsource-roboto-mono/300.css";
 import "fontsource-space-grotesk/500.css";
