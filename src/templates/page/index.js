88c57bc3ce8701a6a33dee5f2327a61b426cfa3f
elysia <elysia.hwang@gmail.com>
2021-01-01 11:56:12 -0500

    Remove unnecessary tags from Page.

--- src/templates/page/index.js
+++ src/templates/page/index.js
@@ -2,14 +2,14 @@ import * as React from "react"
 import Layout from "../../components/layout";
 import Header from "../../components/site-header";
 
+import "./style.scss";
+
 const Page = ({ children }) => {
   return (
-    <>
-      <Layout>
-        <Header />
-        { children }
-      </Layout>
-    </>
+    <Layout>
+      <Header />
+      { children }
+    </Layout>
   )
 }
 
