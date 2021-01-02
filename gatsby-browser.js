1e92969d65997f75a3ee64fba9e17c3287ce5690
elysia <elysia.hwang@gmail.com>
2021-01-01 20:45:44 -0500

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import 'cssremedy/css/remedy.css';
-import 'cssremedy/css/reminders.css';
-import "fontsource-inter";
-import "fontsource-inter/600.css";
-import "fontsource-roboto-mono";
-import "fontsource-roboto-mono/300.css";
-import "fontsource-space-grotesk/500.css";
